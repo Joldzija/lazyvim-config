d5322df3c920a898fb498a5ddf68401c6b9171e1
Joldzija <djoleepythonac@gmail.com>
2025-09-29 22:56:35 +0200

    Add new keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,10 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>qf", ":q<CR>")
+vim.keymap.set("n", "<C-k>", ":m .-2<CR>==")
+vim.keymap.set("n", "<C-j>", ":m .+1<CR>==")
+vim.keymap.set('n', '<leader>wh', ':vertical resize -4<CR>', { noremap = true, silent = true, desc = 'Decrease window width' })
+vim.keymap.set('n', '<leader>wl', ':vertical resize +4<CR>', { noremap = true, silent = true, desc = 'Increase window width' })
+vim.keymap.set('n', '<leader>wk', ':resize -4<CR>', { noremap = true, silent = true, desc = 'Decrease window height' })
+vim.keymap.set('n', '<leader>wj', ':resize +4<CR>', { noremap = true, silent = true, desc = 'Increase window height' })
